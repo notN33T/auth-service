47d3f3cd268330bed5d9f99b00fb22e627737dee
Evgeny Trubish <evgeny.trubish@effective-soft.com>
2022-07-13 18:35:27 +0300

    Add microservice server init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: 'auth_service',
+        url: `0.0.0.0:50052`,
+        protoPath: join(__dirname, '../src/auth/auth.proto'),
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
