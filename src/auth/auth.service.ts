cd64200e1668cd1a53dc30fe876add36350626ea
Evgeny Trubish <evgeny.trubish@effective-soft.com>
2022-07-13 21:25:06 +0300

    Init auth.service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+
+import { CryptService } from '../services/crypto/crypt.service';
+import { JwtService } from '../services/jwt/jwt.service';
+
+import { ClientGrpc } from '@nestjs/microservices';
+import { IGrpcService } from '../user/grpc.interface';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly cryptService: CryptService,
+    private readonly jwtService: JwtService,
+    @Inject('USER_SERVICE') private readonly userService: ClientGrpc,
+  ) {}
+
+  // Connection to user microservice
+  private grpcService: IGrpcService;
+  onModuleInit() {
+    this.grpcService = this.userService.getService<IGrpcService>('UserService');
+  }
+}
