a7673b2759bce332fa10f36d708c941116a8476b
Evgeny Trubish <evgeny.trubish@effective-soft.com>
2022-07-13 22:09:08 +0300

    Implement register logic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
 import { Controller } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
+
+import { AuthService } from './auth.service';
+
+import { RegisterDto } from './dto/register.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @GrpcMethod('AuthService', 'Register')
+  async register(registerDto: RegisterDto): Promise<any> {
+    return this.authService.register(registerDto);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,15 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { lastValueFrom } from 'rxjs';
 
 import { CryptService } from '../services/crypto/crypt.service';
 import { JwtService } from '../services/jwt/jwt.service';
 
 import { ClientGrpc } from '@nestjs/microservices';
 import { IGrpcService } from '../user/grpc.interface';
+
+import { RegisterDto } from './dto/register.dto';
+import { UserServiceCreateResponseDto } from './dto/userServiceRegisterResponse.dto';
+import { Status } from './enums/status.enum';
 
 @Injectable()
 export class AuthService {
@@ -18,5 +23,26 @@ export class AuthService {
   private grpcService: IGrpcService;
   onModuleInit() {
     this.grpcService = this.userService.getService<IGrpcService>('UserService');
+  }
+
+  async register(registerDto: RegisterDto): Promise<any> {
+    const { name, email, password } = registerDto;
+    const hashedPassword = await this.cryptService.hash(password);
+
+    const isUserCreated: UserServiceCreateResponseDto = await lastValueFrom(
+      this.grpcService.CreateUser({ name, email, password: hashedPassword }),
+    );
+
+    const statusFromResponse: Status = isUserCreated.status;
+    const isCreatedStatus = Status[statusFromResponse];
+
+    if (isCreatedStatus === Status[2]) return isUserCreated;
+
+    const [accessToken, refreshToken] = await Promise.all([
+      this.jwtService.generateAccessToken({ name, email }),
+      this.jwtService.generateRefreshToken({ name, email }),
+    ]);
+
+    return { ...isUserCreated, accessToken, refreshToken };
   }
 }
